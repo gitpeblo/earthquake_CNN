dfb5c94324cb01e057145abb82b2ab09b924ba6d
gitpeblo <paolobonfini@virgilio.it>
2025-06-25 18:05:45 +0300

    Add Sphinx documentation scaffold

--- docs/conf.py
+++ docs/conf.py
@@ -24,5 +24,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
+html_title = "Earthquake CNN Documentation"
+#html_logo = "_static/logo.png"   # If you want a custom logo
+html_theme_options = {
+    "sidebar_hide_name": False,
+}
